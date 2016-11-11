3374f60eb64969b1fc5de4baa53cfedd05639c62
Ody Mbegbu <odytrice@gmail.com>
2016-11-11 20:42:50 +0100

    Added Chapter 9 Code

---
+++ ExpertFSharp/Chapter9.fsx
@@ -0,0 +1,36 @@
+﻿type Tree = 
+    | Node of string * Tree * Tree
+    | Tip of string
+
+
+let tree = Node("Letters", Node("Vowels", Tip("A"), Tip("E")), Node("Consonants", Tip("K"), Tip("C")))
+
+let build count tree =
+    
+    let rec loop tree i =
+        if i <= count
+        then loop (Node(i.ToString(),tree, tree)) (i + 1) 
+        else tree
+
+     
+    loop tree 1
+
+
+let rec sizeNotTailRecursive tree = 
+    match tree with
+    | Tip _ -> 1
+    | Node(_, treeLeft, treeRight) -> sizeNotTailRecursive treeLeft + sizeNotTailRecursive treeRight
+
+let rec sizeCont tree cont =
+    match tree with
+    | Tip _ -> cont 1
+    | Node(_, treeLeft, treeRight) -> 
+        sizeCont treeLeft (fun leftSize -> 
+            sizeCont treeRight (fun rightSize -> 
+                cont (leftSize + rightSize)))
+
+let size cont tree = sizeCont tree cont
+
+tree 
+|> build 10
+|> size id
